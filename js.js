771a2d20abc7440d4ce632dedbe13ea650f2e722
Darius Carter <dcarterbusinessemail@gmail.com>
2018-08-13 12:16:29 -0500

    Done !

--- js.js
+++ js.js
@@ -6,4 +6,19 @@ for(var i = 0; i < arrayOfNubers.length; i++)
     stringOfArray += arrayOfNubers[i] + ", ";
 }
 
-document.getElementById("something").textContent= stringOfArray;
+document.getElementById("bHeader").textContent= stringOfArray;
+
+<!--document.getElementById("bHeader").innerText = arrayOfNubers;-->
+
+
+var array0fNames = [];
+array0fNames.push("Jinx");
+array0fNames.push("Kradle");
+array0fNames.push("Draven");
+array0fNames.push("Kratos");
+console.log(array0fNames);
+document.getElementById("nameArray").innerText= array0fNames;
+
+arrayOfNubers.pop(0);
+arrayOfNubers.pop(1);
+document.getElementById("numberDat").textContent = arrayOfNubers;
